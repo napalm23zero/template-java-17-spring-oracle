0c0e1987757151b9d2d29345d0e654141df1db1d
Rodrigo Dantas <rodrigo.dantas@hustletech.dev>
2024-07-11 03:26:22 +0000

    first commit

---
+++ src/main/java/com/hustletech/template_java_17_spring_oracle/TemplateJava17SpringOracleApplication.java
@@ -0,0 +1,13 @@
+package com.hustletech.template_java_17_spring_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateJava17SpringOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateJava17SpringOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hustletech/template_java_17_spring_oracle/TemplateJava17SpringOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hustletech.template_java_17_spring_oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateJava17SpringOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
